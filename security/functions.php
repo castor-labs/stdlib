93ac7f92a7700cf1731d0cbf337c73171b7cc37e
Matias Navarro Carter <mnavarrocarter@gmail.com>
2023-09-22 23:54:20 +0100

    chore: add security functions

--- security/functions.php
+++ security/functions.php
@@ -13,3 +13,41 @@ declare(strict_types=1);
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
+namespace Castor\Security;
+
+use Castor\Context;
+use Castor\MissingContextValue;
+
+/** @internal  */
+enum CtxKey
+{
+    /** @internal  */
+    case Principal;
+}
+
+function withPrincipal(Context $ctx, Principal $principal): Context
+{
+    return Context\withValue($ctx, CtxKey::Principal, $principal);
+}
+
+function getPrincipal(Context $ctx): Principal
+{
+    return $ctx->value(CtxKey::Principal) ??
+        throw MissingContextValue::forKey(CtxKey::Principal);
+}
+
+function lookupPrincipal(Context $ctx): ?Principal
+{
+    return $ctx->value(CtxKey::Principal);
+}
+
+function getIdentity(Context $ctx): Identity
+{
+    return getPrincipal($ctx)->getIdentity();
+}
+
+function lookupIdentity(Context $ctx): ?Identity
+{
+    return lookupPrincipal($ctx)?->getIdentity();
+}
